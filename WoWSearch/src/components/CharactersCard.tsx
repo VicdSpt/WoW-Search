16c5d519991eeb510490592c24c48c6d595093cf
Victor dS <victordespirlet@gmail.com>
2026-01-18 17:33:11 +0100

    building a wow characters search

---
+++ WoWSearch/src/components/CharactersCard.tsx
@@ -0,0 +1,43 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardActions from '@mui/material/CardActions';
+
+interface Character {
+    id: number;
+    name: string,
+    race: string;
+    class: string;
+    faction: string;
+    description: string;
+    image: string;
+}
+interface CharacterCardProps{
+    character: Character;
+}
+
+export default function MultiActionAreaCard({character}: CharacterCardProps) {
+  return (
+    <Card className='my-5 '>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          image={character.image}
+          alt={`${character.name} - ${character.race} ${character.class}`}
+        />
+        <CardContent>
+          <Typography>{character.name}</Typography>
+          <Typography>{character.race} {character.class}</Typography>
+          <Typography>{character.faction}</Typography>
+          <Typography>{character.description}</Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button>Add to Favorite</Button>
+      </CardActions>
+    </Card>
+  );
+}
